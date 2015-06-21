5e739530652dc5d204903285e878205ee5d0428d
BigKBear <kylesthill@hotmail.com>
2015-06-21 08:41:04 -0400

    Transfer

--- CustomMathTest.php
+++ CustomMathTest.php
@@ -4,7 +4,7 @@ class CustomMath {
 	public function add($x, $y) {
 		if ((is_string($x))&&(($x!=0))) {
 			throw new InvalidArgumentException('Expected first value to be number');
-		} else if ((is_string($x))&&((is_string($y))))&&(($x!=0)&&($y!=0)) {
+		} else if ((is_string($x))||((is_string($y))))&&(($x!=0)||($y!=0)) {
 			throw new InvalidArgumentException('Expected first value to be number');
 		} else if ($x == NULL) {
 			throw new InvalidArgumentException('Expected First value to be number');
