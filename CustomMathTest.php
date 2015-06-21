ab4152e48531e53575575ec6f499dc1c7fe962a0
Kyle <kylesthill@hotmail.com>
2015-06-21 10:38:41 -0700

    transfer

--- CustomMathTest.php
+++ CustomMathTest.php
@@ -8,7 +8,7 @@ class CustomMath {
                         throw new InvalidArgumentException('Expected first value to be number');
                 } else if ((is_string($x))||(($x!=0))) {
 			throw new InvalidArgumentException('Expected first value to be number');
-		} else if (($x===0)&&((((is_string($y))))||($y!=0))) {
+		} else if (($x===0)&&((is_string($y)))) {
 			throw new InvalidArgumentException('Expected Second value to be number');
 		} else {
 			$answer = $x + $y;
