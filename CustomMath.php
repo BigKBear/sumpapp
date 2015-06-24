8ab6007b54a7cc520f1c68d4a55396d3a57f07c5
Kyle St.Hill <kyle.sthill@hyuna.bb>
2015-06-24 10:58:54 -0400

    Front end and original logic implemented still need to fix 2 test and include code coverage

--- CustomMath.php
+++ CustomMath.php
@@ -7,23 +7,17 @@
 
 class CustomMath {
 	public function add($x, $y) {
-		//var_dump($x); 
+		//var_dump($x);
 		if (($x === 0) && ($y === 0)) {
-			return "0";
-		} else if (($x === 0) && (($y > 0) || ($y < 0))) {
+			return 0;
+		} else if ((is_numeric($x)) && (is_numeric($y))) {
 			return $x + $y;
-		} else if ($y == NULL) {
+		} else if ((is_string($y)) || ($y == null)) {
 			throw new InvalidArgumentException('Expected Second value to be number');
-		} else if (($x === 0) && (is_string($y))) {
-			throw new InvalidArgumentException('Expected Second value to be number');
-		} else if ($x == NULL) {
+			return ;
+		} else if ((is_string($x)) || ($x == null)) {
 			throw new InvalidArgumentException('Expected first value to be number');
-		} else if (is_string($x)) {
-			throw new InvalidArgumentException('Expected first value to be number');
-		} else if (is_string($y)) {
-			throw new InvalidArgumentException('Expected Second value to be number');
-		} else {
-			return $x + $y;
+			return ;
 		}
 	}
 
